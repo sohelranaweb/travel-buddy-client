01077c06c2667e702b9f57cb7ac74e35b5831f15
sohelranaweb <sohel.rana.web2@gmail.com>
2025-12-17 09:15:54 +0600

    url change

--- src/lib/server-fetch.ts
+++ src/lib/server-fetch.ts
@@ -1,8 +1,7 @@
 import { getNewAccessToken } from "@/services/auth/auth.service";
 import { getCookie } from "@/services/auth/tokenHandlers";
 
-const BACKEND_API_URL =
-  process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:5000/api/v1";
+const BACKEND_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
 // /auth/login
 const serverFetchHelper = async (
